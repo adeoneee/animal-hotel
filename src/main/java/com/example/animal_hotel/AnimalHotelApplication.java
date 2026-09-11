70e1e057a9ad34d51215229e3ac5d5a1acdc9a57
adeoneee <da.se73@mail.ru>
2026-09-11 23:48:44 +0400

    first commit

---
+++ src/main/java/com/example/animal_hotel/AnimalHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/animal_hotel/AnimalHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
